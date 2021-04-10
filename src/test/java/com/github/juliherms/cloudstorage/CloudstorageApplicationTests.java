eb0075984da5fc141b604e40efbd9e5d38575a16
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-04-10 16:24:32 -0300

    first commit

---
+++ src/test/java/com/github/juliherms/cloudstorage/CloudstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juliherms.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
